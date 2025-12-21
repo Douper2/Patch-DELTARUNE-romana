6fc91ded5c974af8bd1aa0cf62b4dddf345894a1
Douper <106928620+Douper2@users.noreply.github.com>
2025-12-21 19:11:02 +0200

    adaugare imagine + text (mai am de testat)

--- patch_deltromana.py
+++ patch_deltromana.py
@@ -1,6 +1,7 @@
-import tkinter, os, snfi
-import tkinter.filedialog, requests
-from tkinter import ttk, PhotoImage, Canvas
+import tkinter, os, snfi, requests
+from tkinter import *
+from tkinter import ttk
+import PIL.Image, PIL.ImageTk
 
 foldel = r"C:\Program Files (x86)\Steam\steamapps\common\DELTARUNE"
 
@@ -9,9 +10,15 @@ m = tkinter.Tk()
 m.geometry("640x480")
 cad = ttk.Frame(m, padding=10)
 butoninceput = ttk.Button(m, text="Instalează", width=12, command=snfi.patching)
-# imagine = PhotoImage(file="tenna_sprite_installer.png")
-# imaginepgr = tkinter.Label(m, imagine)                       DE ADAUGĂT IMAGINE ÎN STÂNGA PROGRAMULUI
-# imaginepgr.pack()
+imagine = PIL.Image.open("tenna_sprite_installer.png")
+imaginepgr = PIL.ImageTk.PhotoImage(imagine)
+imgpgr = ttk.Label(m, image=imaginepgr)
+imgpgr.pack()
+
+textdetest = ttk.Label(m, text="Bine ai venit la aplicația oficială de instalare a traducerii DELTARUNE în limba română!\n"
+                               "Pentru a începe, apasă butonul pe care scrie 'Instalează' și totul va fi instalat rapid și\nautomat", font=("Arial", 12))
+textdetest.pack()
+
 butoninceput.place(x=540, y=410)
 
 m.title("Mod DELTARUNE Română")
